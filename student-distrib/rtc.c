145e9e4636fcc8d14370987cf09a378fcf313056
Ryan Haskell-Glatz <hskllgl2@illinois.edu>
2014-03-17 01:27:02 +0000

    Added rtc functions
    git-svn-id: https://subversion.ews.illinois.edu/svn/sp14-ece391/_projects/group14@13209 1cec0ab5-dbbe-4d55-bb2a-e14ca091d7d0

---
+++ student-distrib/rtc.c
@@ -0,0 +1,10 @@
+/* rtc.c - Functions to interact with the RTC controller
+ * vim:ts=4 noexpandtab
+ */
+
+#include "rtc.h"
+#include "lib.h"
+
+ //	TODO
+
+ //	RTC STUFFS
---
+++ student-distrib/rtc.h
@@ -0,0 +1,14 @@
+/* rtc.h - Defines used in interactions with the RTC controller
+ * vim:ts=4 noexpandtab
+ */
+ 
+#ifndef _RTC_H
+#define _RTC_H
+
+#include "types.h"
+
+//	TODO
+
+//	RTC STUFFS
+
+#endif /* _RTC_H */
