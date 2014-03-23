ba2b57b4489b763794fe550d4470701d4b0a4b83
Adam Madsen <madsen.adam@gmail.com>
2014-03-23 23:20:01 +0000

    Fix email script so that it rewrites by name as well.
    git-svn-id: https://subversion.ews.illinois.edu/svn/sp14-ece391/_projects/group14/trunk@14674 1cec0ab5-dbbe-4d55-bb2a-e14ca091d7d0

--- git-fix-email.sh
+++ git-fix-email.sh
@@ -6,10 +6,10 @@ n=$GIT_AUTHOR_NAME
 m=$GIT_AUTHOR_EMAIL
 
 case ${GIT_AUTHOR_NAME} in
-  ajmadse2) n="Adam Madsen" ; m="madsen.adam@gmail.com" ;;
-  hskllgl2) n="Ryan Haskell-Glatz" ; m="hskllgl2@illinois.edu" ;;
-  taberna2) n="Artur Tabernacki" ; m="atabernacki@gmail.com" ;;
-  vinjamu2) n="Raj Vinjamuri" ; m="rajvinjamuri@gmail.com" ;;
+  ajmadse2|"Adam Madsen") n="Adam Madsen" ; m="madsen.adam@gmail.com" ;;
+  hskllgl2|"Ryan Haskell-Glatz") n="Ryan Haskell-Glatz" ; m="hskllgl2@illinois.edu" ;;
+  taberna2|"Artur Tabernacki") n="Artur Tabernacki" ; m="atabernacki@gmail.com" ;;
+  vinjamu2|"Raj Vinjamuri") n="Raj Vinjamuri" ; m="rajvinjamuri@gmail.com" ;;
 esac
 
 export GIT_AUTHOR_NAME="$n"
