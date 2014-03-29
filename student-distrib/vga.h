ead1886d67e990605a3a6aeaeae00e5b2757e231
Adam Madsen <madsen.adam@gmail.com>
2014-03-29 09:44:37 +0000

    Implement method to set vga cursor position; cursor now follows text
    git-svn-id: https://subversion.ews.illinois.edu/svn/sp14-ece391/_projects/group14/branches/terminal-driver@14980 1cec0ab5-dbbe-4d55-bb2a-e14ca091d7d0

---
+++ student-distrib/vga.h
@@ -0,0 +1,28 @@
+/* vga.h - defines functions for manipulating VGA memory and registers
+ * vim:ts=4 sw=4 noexpandtab
+ */
+#ifndef _VGA_H_
+#define _VGA_H_
+
+#include "types.h"
+
+/* General constants related to video memory */
+#define NUM_COLS 80
+#define NUM_ROWS 25
+
+#define VIDEO 0xB8000
+
+/* Values related to VGA CRTC registers */
+#define VGA_CRTC_ADDRESS	0x3D4
+#define VGA_CRTC_DATA		0x3D5
+
+#define VGA_CURSOR_LOC_LOW	0x0F
+#define VGA_CURSOR_LOC_HIGH	0x0E
+
+/* c structs go here */
+#ifndef ASM
+
+extern void vga_cursor_set_location(uint8_t row, uint8_t col);
+#endif /* ASM */
+
+#endif /* _VGA_H_ */
