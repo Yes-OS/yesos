ea2c2b51530a38f3a661b8dfb586f9c27e8571f1
Adam Madsen <madsen.adam@gmail.com>
2014-03-31 06:35:56 +0000

    Move some VGA-specific variables and routines to the VGA-specific files
    git-svn-id: https://subversion.ews.illinois.edu/svn/sp14-ece391/_projects/group14/branches/terminal-driver@15232 1cec0ab5-dbbe-4d55-bb2a-e14ca091d7d0

--- student-distrib/vga.c
+++ student-distrib/vga.c
@@ -4,6 +4,10 @@
 #include "lib.h"
 #include "vga.h"
 
+int screen_x;
+int screen_y;
+char* video_mem = (char *)VIDEO;
+
 /* Sets the vga cursor to the specified position */
 void vga_cursor_set_location(uint8_t row, uint8_t col)
 {
@@ -13,3 +17,10 @@ void vga_cursor_set_location(uint8_t row, uint8_t col)
 	outb(VGA_CURSOR_LOC_HIGH, VGA_CRTC_ADDRESS);
 	outb((address >> 8) & 0x0FF, VGA_CRTC_DATA);
 }
+
+/* Sets the vga cursor to the current soft location of video memory */
+void update_cursor(void)
+{
+	vga_cursor_set_location(screen_y, screen_x);
+}
+
