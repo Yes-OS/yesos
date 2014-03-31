44e716564643141ed31e07a8c92a1cc8ffdf03f7
Raj Vinjamuri <rajvinjamuri@gmail.com>
2014-03-31 19:48:28 +0000

    Update return conditions and functionality framework
    git-svn-id: https://subversion.ews.illinois.edu/svn/sp14-ece391/_projects/group14/branches/file_system@15368 1cec0ab5-dbbe-4d55-bb2a-e14ca091d7d0

--- student-distrib/file_sys.c
+++ student-distrib/file_sys.c
@@ -1,5 +1,6 @@
 /* file_sys.c - Functions to initialize and interact
  *              with the file system.
+ * 
  * vim:ts=4 sw=4 noexpandtab
  */
 
@@ -51,40 +52,93 @@ uint32_t fs_close(/**/)
 
 /*	Read Directory Entry by Name
  *	Parameters:	fname 	- the name of the file.
- *			dentry	- pointer to directory entry of file to set name.
- *	Return -1 on failure.
+ *				dentry	- pointer to directory entry of file to set name.
+ *
+ *  Fill 'dentry_t' block passed in with:
+ *		-file name, file type, and inode number
+ *
+ *	Return 0 on success, -1 on failure.
  *	
  */
 int32_t read_dentry_by_name (const uint8_t* fname, dentry_t* dentry)
 {
-	(*dentry).file_name = fname;
+	//Check for non-existant file or invalid index
+	if(true)
+	{
+		return -1;
+	}
+
+//	dentry->file_name = fname;
+
+// use memcpy
+
 	return 0;
 }
 
-/*	Read Directory Entry by Index
- *	Parameters:	index	- the index of the file.
- *			dentry	- pointer to directory entry of file to set index.
- *	Return -1 on failure.
+/*	Read Directory Entry by Name
+ *	Parameters:	fname 	- the name of the file.
+ *				dentry	- pointer to directory entry of file to set name.
+ *
+ *  Fill 'dentry_t' block passed in with:
+ *		-file name, file type, and inode number
+ *
+ *	Return 0 on success, -1 on failure.
  *	
  */
 int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry)
 {
-	//	!!	Check that index is less than number of indices (boot block)
+
+//	!!	Check that index is less than number of indices (boot block)
 //	if(index < <global boot block>.numnodes)
-		(*dentry).inode_num = index;
-	else return -1;
+//		dentry->inode_num = index;
+
+	//Check for non-existant file or invalid index
+	if(true)
+	{
+		return -1;
+	}
+
+
+// use memcpy
+
 	return 0;
 }
 
 /*	Read Data
  *	Parameters:	inode	- the index node of the data.
- *			offset	- how many bytes to begin reading at?
- *			buf	- data array to read.
- *			length - size of array.
- *	Return -1 on failure.
+ *				offset	- how many bytes to begin reading at?
+ *				buf		- data array to read.
+ *				length  - size of array.
+ *
+ *  Reads up to 'length' bytes from position 'offset'
+ *	  in the file with 'inode' number into the given 'buf' buffer.
+ *
+ *	Return  # of bytes read and placed into buffer
+ *			0 when end of file reached.
+ *			-1 on failure.
  *
  */
 int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length)
 {
-	return -1;
+	unsigned int num_bytes_read;
+	unsigned int end_of_file; //placeholder
+	
+	num_bytes_read = 1234;
+	end_of_file = 1;
+	
+	//Check that the given inode is within the valid range
+	if(true)
+	{
+		return -1
+	}
+
+// use memcpy
+
+	if(end_of_file)
+	{
+		return 0;
+	}
+	
+	return (num_bytes_read);
+
 }
--- student-distrib/file_sys.h
+++ student-distrib/file_sys.h
@@ -13,6 +13,7 @@
 #ifndef ASM
 
 #define FILE_NAME_SIZE 		32;
+
 #define FILE_TYPE_RTC		0;
 #define FILE_TYPE_DIR		1;
 #define FILE_TYPE_REG		2;
@@ -31,7 +32,7 @@ typedef struct file {
     uint32_t inode_ptr;
     uint32_t file_pos;
     uint32_t flags;
-} FILE;
+} file_t;
 
 /*	Index Node Struct
  *	4096-bytes
@@ -42,7 +43,7 @@ typedef struct index_node
 	//	4 bytes per data block #
 
 	uint32_t block_length;
-	uint32_t * data_blocks;	//used to traverse blocks
+	uint32_t * data_block;	//used to traverse blocks
 
 } index_node_t;
 
@@ -53,8 +54,8 @@ typedef struct index_node
 typedef struct dentry
 {
 	//	32 bytes - file name
-	//	4 bytes - files type	(0, 1, or 2)
-	//	4 bytes - inode number (NULL for type 0 and 1)
+	//	4 bytes	 - files type	(0, 1, or 2)
+	//	4 bytes	 - inode number (ignored for type 0 and 1)
 	//	24 bytes - reserved 
 
 	uint8_t * file_name;
@@ -73,12 +74,12 @@ typedef struct boot_block
 	//	4 bytes - number of index nodes (inodes)
 	//	4 bytes - number of data blocks
 	//	52 bytes - reserved
-	//	64 bytes - directory entries
+	//	64-byte directory entries
 
 	uint32_t num_entries;
 	uint32_t num_nodes;
 	uint32_t num_blocks;
-	dentry_t * entries;
+	dentry_t * entries; //used to traverse dir. entries
 	//	array size stored in 'num_entries'
 		
 } boot_block_t;
@@ -113,27 +114,35 @@ uint32_t fs_close(/**/);
 
 /*	Read Directory Entry by Name
  *	Parameters:	fname 	- the name of the file.
- *			dentry	- pointer to directory entry of file to set name.
- *	Return -1 on failure.
+ *				dentry	- pointer to directory entry of file to set name.
+ *
+ *  Fill 'dentry_t' block passed in with:
+ *		-file name, file type, and inode number
+ *
+ *	Return 0 on success, -1 on failure.
  *	
  */
 int32_t read_dentry_by_name(const uint8_t* fname, dentry_t* dentry);
 
 
-/*	Read Directory Entry by Index
- *	Parameters:	index	- the index of the file.
- *			dentry	- pointer to directory entry of file to set index.
- *	Return -1 on failure.
+/*	Read Directory Entry by Name
+ *	Parameters:	fname 	- the name of the file.
+ *				dentry	- pointer to directory entry of file to set name.
+ *
+ *  Fill 'dentry_t' block passed in with:
+ *		-file name, file type, and inode number
+ *
+ *	Return 0 on success, -1 on failure.
  *	
  */
 int32_t read_dentry_by_index(uint32_t index, dentry_t* dentry);
 
 /*	Read Data
  *	Parameters:	inode	- the index node of the data.
- *			offset	- how many bytes to begin reading at?
- *			buf	- data array to read.
- *			length - size of array.
- *	Return -1 on failure.
+ *				offset	- how many bytes to begin reading at?
+ *				buf		- data array to read.
+ *				length  - size of array.
+ *	Return 0 on success, -1 on failure.
  *
  */
 int32_t read_data(uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length);
