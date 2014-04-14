d3c29ac3c31e4676cc42f84ba2e7f6b46a66ad8c
Adam Madsen <madsen.adam@gmail.com>
2014-04-14 16:35:12 -0500

    Add command to fetch PCB of current process

--- student-distrib/syscall_impl.c
+++ student-distrib/syscall_impl.c
@@ -2,8 +2,24 @@
  * vim:ts=4 sw=4 noexpandtab
  */
 #include "types.h"
-#include "syscall.h"
 #include "lib.h"
+#include "proc.h"
+#include "term.h"
+#include "file_sys.h"
+#include "syscall.h"
+
+#define MAX_CMD_LEN 33
+
+/* Gets the process's PCB */
+static inline pcb_t *get_proc_pcb()
+{
+	uint32_t pcb;
+	asm (	"movl	$0xFFFFE000, %0		\n\
+			 andl	%%esp, %0"
+			: "=r"(pcb)
+			: : );
+	return (pcb_t *)pcb;
+}
 
 int32_t sys_open(const uint8_t *filename)
 {
@@ -27,6 +43,14 @@ int32_t sys_close(int32_t fd)
 
 int32_t sys_exec(const uint8_t *command)
 {
+	uint8_t file[MAX_CMD_LEN];
+	int i;
+	const uint32_t *c;
+
+	for (i = 0, c = command; i < MAX_CMD_LEN && c == ' '; i++, c++) {
+
+	}
+
 	return 0;
 }
 
