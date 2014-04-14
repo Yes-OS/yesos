a2e13acb87041e0ed5f65ec416acc59217677d37
Adam Madsen <madsen.adam@gmail.com>
2014-04-14 16:46:48 -0500

    Add typedefs for function pointers

--- student-distrib/syscall_impl.c
+++ student-distrib/syscall_impl.c
@@ -10,6 +10,11 @@
 
 #define MAX_CMD_LEN 33
 
+typedef int32_t open_t(const uint8_t *filename);
+typedef int32_t read_t(int32_t fd, void *buf, int32_t nbytes);
+typedef int32_t write_t(int32_t fd, const void *buf, int32_t nbytes);
+typedef int32_t close_t(int32_t fd);
+
 /* Gets the process's PCB */
 static inline pcb_t *get_proc_pcb()
 {
@@ -45,11 +50,12 @@ int32_t sys_exec(const uint8_t *command)
 {
 	uint8_t file[MAX_CMD_LEN];
 	int i;
-	const uint32_t *c;
+	const uint8_t *c;
 
-	for (i = 0, c = command; i < MAX_CMD_LEN && c == ' '; i++, c++) {
-
+	for (i = 0, c = command; i < MAX_CMD_LEN && *c == (uint8_t)' '; i++, c++) {
+		/* skip beginning spaces */
 	}
+
 
 	return 0;
 }
